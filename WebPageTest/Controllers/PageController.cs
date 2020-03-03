75b13dac8cdaaa800d9c21c192767f68e7e753eb
Fernando Solano <fesolano@outcoding.com>
2020-03-03 14:07:02 -0600

    first commit

---
+++ WebPageTest/Controllers/PageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace WebPageTest.Controllers
+{
+    public class PageController : Controller
+    {
+        public void saveWebPageTest()
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(localhost)\SQLEXPRESS;Initial Catalog=WebPageTest;Integrated Security=True");
+            SqlDataAdapter sqlda = new SqlDataAdapter("Select * from Test", sqlCon);
+            DataTable dtbl = new DataTable();
+            sqlda.Fill(dtbl);
+            foreach (DataRow row in dtbl.Rows)
+            {
+                Console.WriteLine(row["ProductName"]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebPageTest/Models/Page.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace WebPageTest.Models
+{
+    public class Page
+    {
+        public string Url { get; set; }
+
+        public string Location { get; set; }
+
+        public string Browser { get; set; }
+
+        public string Connectivity { get; set; }
+
+        public string runs { get; set; }
+
+        public string DownloadBandwidth { get; set; }
+
+        public string UploadBandwidth { get; set; }
+
+        public string Latency { get; set; }
+
+        public int ReapeatView { get; set; }
+
+        public int isMobile { get; set; }
+
+        public string FormatConnectivity()
+        {
+            return $"{Location}:{Browser}.{Connectivity}";
+        }
+    }
+}
