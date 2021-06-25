0b4f2c950f8a70cb1edae17076b7ce40c40eadc6
Fernando Solano <fersolano@ex2outcoding.local>
2021-06-24 21:49:01 -0600

    Remove using to used

--- WebPageTest/Controllers/HomeController.cs
+++ WebPageTest/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace WebPageTest.Controllers
